9d361e37c79a9bf262bd2aab91f1d24c530d1252
Igor <igorbojk@gmail.com>
2018-03-29 17:36:32 +0300

    add house-section

--- src/build/js/libs.js
+++ src/build/js/libs.js
@@ -3,4 +3,8 @@ $( document ).ready(function() {
     $('#carouselExampleControls').carousel({
         interval: 6000
     })
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
 });
