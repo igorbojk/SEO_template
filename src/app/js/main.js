afa8d036fc1aa88c841db5d6be88968da12d9fa2
Igor <igorbojk@gmail.com>
2018-04-02 10:33:11 +0300

    add slick slider

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,10 +1,23 @@
 
 $( document ).ready(function() {
     $('#carouselExampleControls').carousel({
-        interval: 6000
-    })
+        interval: 6000,
+        wrap: false,
+        ride: false
+    });
     $(document).on('click', '[data-toggle="lightbox"]', function(event) {
         event.preventDefault();
         $(this).ekkoLightbox();
     });
+
+    $('.advice-slider').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next')
+    });
+
 });
