f8696f2cd2b966378746516b79b0464bde224558
Igor <igorbojk@gmail.com>
2018-04-04 10:51:03 +0300

    add slider animation

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,15 +1,57 @@
 
 $( document ).ready(function() {
-    $('#carouselExampleControls').carousel({
-        interval: 6000,
-        wrap: false,
-        ride: false
+
+    $('.priorities-image-slider').slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        asNavFor: '.annotation-slider',
+        prevArrow: $('.carousel-control-prev'),
+        nextArrow: $('.carousel-control-next')
+        // responsive: [
+        //     {
+        //         breakpoint: 992,
+        //         settings: {
+        //             slidesToShow: 3
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 576,
+        //         settings: {
+        //             slidesToShow: 2
+        //         }
+        //     }
+        // ]
     });
-    $('#mobileCarousel').carousel({
-        interval: 6000,
-        wrap: false,
-        ride: false
+
+    $('.annotation-slider').slick({
+        infinite: false,
+        fade: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        asNavFor: '.priorities-image-slider',
+        prevArrow: $('.carousel-control-prev'),
+        nextArrow: $('.carousel-control-next')
+        // responsive: [
+        //     {
+        //         breakpoint: 992,
+        //         settings: {
+        //             slidesToShow: 3
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 576,
+        //         settings: {
+        //             slidesToShow: 2
+        //         }
+        //     }
+        // ]
     });
+
     $(document).on('click', '[data-toggle="lightbox"]', function(event) {
         event.preventDefault();
         $(this).ekkoLightbox();
