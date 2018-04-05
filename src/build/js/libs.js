93fa0271d5a114f358ee0d56143f5e2517a0dce0
Igor <igorbojk@gmail.com>
2018-04-05 12:46:58 +0300

    fix slider

--- src/build/js/libs.js
+++ src/build/js/libs.js
@@ -1,104 +1,108 @@
 
 $( document ).ready(function() {
 
-    $('.priorities-image-slider').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: false,
-        asNavFor: '.annotation-slider',
-        prevArrow: $('.carousel-control-prev'),
-        nextArrow: $('.carousel-control-next')
-    });
+    setTimeout(function() {
+        $('.priorities-image-slider').slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            asNavFor: '.annotation-slider',
+            prevArrow: $('.carousel-control-prev'),
+            nextArrow: $('.carousel-control-next')
+        });
 
-    $('.annotation-slider').slick({
-        infinite: false,
-        fade: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true,
-        asNavFor: '.priorities-image-slider',
-        prevArrow: $('.carousel-control-prev'),
-        nextArrow: $('.carousel-control-next')
-    });
+        $('.annotation-slider').slick({
+            infinite: false,
+            fade: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            asNavFor: '.priorities-image-slider',
+            prevArrow: $('.carousel-control-prev'),
+            nextArrow: $('.carousel-control-next')
+        });
 
-    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-        event.preventDefault();
-        $(this).ekkoLightbox();
-    });
+        $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+            event.preventDefault();
+            $(this).ekkoLightbox();
+        });
 
-    $('.club-house-slider').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        dots: false,
-        arrows: true,
-        prevArrow: $('.club-house-slider-container .carousel-control-prev'),
-        nextArrow: $('.club-house-slider-container .carousel-control-next')
-    });
+        $('.club-house-slider').slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            dots: false,
+            arrows: true,
+            prevArrow: $('.club-house-slider-container .carousel-control-prev'),
+            nextArrow: $('.club-house-slider-container .carousel-control-next')
+        });
 
-    $('.clubs-life-slider').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        dots: false,
-        arrows: true,
-        prevArrow: $('.clubs-life-slider-prev'),
-        nextArrow: $('.clubs-life-slider-next')
-    });
+        $('.clubs-life-slider').slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            dots: false,
+            arrows: true,
+            prevArrow: $('.clubs-life-slider-prev'),
+            nextArrow: $('.clubs-life-slider-next')
+        });
 
-    $('.advice-slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true,
-        infinite: true,
-        centerMode: true,
-        prevArrow: $('.advice-slider-prev-button'),
-        nextArrow: $('.advice-slider-next-button'),
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 3
+        $('.advice-slider').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            infinite: true,
+            centerMode: true,
+            prevArrow: $('.advice-slider-prev-button'),
+            nextArrow: $('.advice-slider-next-button'),
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1
+                    }
                 }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+            ]
+        });
 
-    $('.projects-slider').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true,
-        prevArrow: $('.projects-slider-prev-button'),
-        nextArrow: $('.projects-slider-next-button'),
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 3
+        $('.projects-slider').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            prevArrow: $('.projects-slider-prev-button'),
+            nextArrow: $('.projects-slider-next-button'),
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 2
+                    }
                 }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 2
-                }
-            }
-        ]
-    });
+            ]
+        });
+    }, 2500);
+
+
 
     window.onscroll=function(){
         console.log('no-scroll');
